5887fca946e135a7ba2558887491e3faeb6cd2c9
Diego Martinez <Diego@Diegos-MacBook-Pro.local>
2019-10-28 17:26:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app and shotgun on port 1126!!!! "
   end
 
 end
